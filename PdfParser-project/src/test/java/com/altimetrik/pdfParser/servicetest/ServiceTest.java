7070ae446849621b4fbdc21c00c25e50c1e3465a
Anubhav kumar <anubhavcse08@users.noreply.github.com>
2019-07-07 10:17:20 +0530

    Update ServiceTest.java

--- PdfParser-project/src/test/java/com/altimetrik/pdfParser/servicetest/ServiceTest.java
+++ PdfParser-project/src/test/java/com/altimetrik/pdfParser/servicetest/ServiceTest.java
@@ -26,5 +26,53 @@ public class ServiceTest {
 		String invoiceNum = "invNo";
 		assertEquals("Invoice no. not found.", readPdfData.findInvoiceNo(invoiceNum));
 	}
+	
+	@Test(expected=DatabaseException.class)
+	public void addOneTest() throws DatabaseException
+	{
+		Invoice invoice = new Invoice();
+		invoice.setAddress("sfsdf");
+		invoice.setAmount(23423);
+		invoice.setCustomerPO("sdfsdg");
+		invoice.setApproved(true);
+		dao.addToDatabase(invoice);
+		
+	}
+	
+	@Test
+	public void getOneTest() throws DatabaseException 
+	{
+		Invoice invoice = new Invoice();
+		invoice.setInvoiceNo("24442342");
+		invoice.setAddress("sfsdf");
+		invoice.setAmount(23423);
+		invoice.setInvoiceDate(LocalDate.now());
+		invoice.setCustomerPO("sdfsdg");
+		invoice.setApproved(true);
+		invoice.setEmail("pdfparser12345gmail.com");
+		dao.addToDatabase(invoice);
+		assertEquals(invoice.getInvoiceNo(),dao.getFromDatabase(invoice.getInvoiceNo()).getInvoiceNo());
+		//assertEquals(invoice.getInvoiceNo(),dao.getFromDatabase(invoice.getInvoiceNo()));
+		
+		
+		
+	}
+	@Test
+	public void approveInvoiceTest() throws DatabaseException 
+	{
+		Invoice invoice = new Invoice();
+		invoice.setInvoiceNo("24442346");
+		invoice.setAddress("sfsdf");
+		invoice.setAmount(23423);
+		invoice.setInvoiceDate(LocalDate.now());
+		invoice.setCustomerPO("sdfsdg");
+		invoice.setApproved(false);
+		invoice.setEmail("pdfparser12345gmail.com");
+		dao.addToDatabase(invoice);
+		dao.approveInvoice(invoice.getInvoiceNo());
+		Invoice obj = dao.getFromDatabase(invoice.getInvoiceNo());
+		assertTrue(obj.isApproved());
+		
+	}
 
 }
